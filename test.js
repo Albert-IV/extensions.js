ecd9fc56696f02809fe133b5637d4968deca3206
alberte <dropped.on.japan@gmail.com>
2013-08-12 12:13:40 -0500

    Updated test JS to have new test.

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 var labelCheck = new Date().getLabel,
-    mergeCheck = [].merge;
+    mergeCheck = [].merge,
+    isDateCheck = new Date().isDate;
 
 var arr1 = [1,2,3,4,5],
     arr2 = [6,7,8,9,0];
 
 document.getElementById('get-label').innerHTML = labelCheck && new Date().getLabel() || labelCheck;
 document.getElementById('merge').innerHTML = mergeCheck && arr1.merge(arr2) || mergeCheck;
+document.getElementById('is-date').innerHTML = isDateCheck && ( 'Date is valid: ' + new Date().isDate()) || isDateCheck;
