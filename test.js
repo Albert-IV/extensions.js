3c8d9d910c37909b3d8c959ff652087f7b4eb73d
alberte <dropped.on.japan@gmail.com>
2013-08-12 16:49:37 -0500

    Added tests for Object.get() method, refactored innerHTML

--- test.js
+++ test.js
@@ -1,10 +1,34 @@
 var labelCheck = new Date().getLabel,
     mergeCheck = [].merge,
-    isDateCheck = new Date().isDate;
+    isDateCheck = new Date().isDate,
+    getCheck = {}.get;
 
 var arr1 = [1,2,3,4,5],
-    arr2 = [6,7,8,9,0];
+    arr2 = [6,7,8,9,0],
+    deepObj = {
+      name: [
+        {
+          first: 'Jen',
+          last: 'Powers'
+        },
+        {
+          first: 'Ken',
+          last: 'Powers'
+        }
+      ]
+    };
 
-document.getElementById('get-label').innerHTML = labelCheck && new Date().getLabel() || labelCheck;
-document.getElementById('merge').innerHTML = mergeCheck && arr1.merge(arr2) || mergeCheck;
-document.getElementById('is-date').innerHTML = isDateCheck && ( 'Date is valid: ' + new Date().isDate()) || isDateCheck;
+document.getElementById('get-label').innerHTML =
+  labelCheck && new Date().getLabel() || labelCheck;
+
+document.getElementById('merge').innerHTML =
+  mergeCheck && arr1.merge(arr2) || mergeCheck;
+
+document.getElementById('is-date').innerHTML =
+  isDateCheck && ( 'Date is valid: ' + new Date().isDate()) || isDateCheck;
+
+document.getElementById('success-get').innerHTML =
+  getCheck && deepObj.get('name[0].last') || getCheck;
+
+document.getElementById('fail-get').innerHTML =
+  getCheck & deepObj.get('some.unknown[key].here');
