0ec9cf84e2ae1d1b553ba749b0687bdebbf73226
alberte <dropped.on.japan@gmail.com>
2013-08-22 10:47:58 -0500

    Added check for String.prototype.formatPhone.

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 var labelCheck = new Date().getLabel,
     mergeCheck = [].merge,
     isDateCheck = new Date().isDate,
-    getCheck = {}.get;
+    getCheck = {}.get,
+    phoneLabelCheck = ''.formatPhone;
 
 var arr1 = [1,2,3,4,5],
     arr2 = [6,7,8,9,0],
+    phoneString = "+(573)321-1124",
     deepObj = {
       name: [
         {
@@ -31,4 +33,8 @@ document.getElementById('success-get').innerHTML =
   getCheck && deepObj.get('name[0].last') || getCheck;
 
 document.getElementById('fail-get').innerHTML =
-  getCheck & deepObj.get('some.unknown[key].here');
+  getCheck && deepObj.get('some.unknown[key].here');
+
+document.getElementById('phone-label').innerHTML =
+  phoneLabelCheck && phoneString.formatPhone() || phoneLabelCheck;
+  
