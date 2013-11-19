9bf683703552d67eadaf3c08fb031e893613a561
Albert Engelbrecht <dropped.on.japan@gmail.com>
2013-11-19 16:11:47 -0600

    Update extensions.js
    Fixed a typo I just seent.
    
    http://picsthatdontsuck.com/img/I_seent_it.jpg

--- extensions.js
+++ extensions.js
@@ -178,7 +178,7 @@
 
     extendMe = function(nativeObj, extensions) {
       for (var extension in extensions) {
-        obj.prototype[extension] = extensions[extension];
+        nativeObj.prototype[extension] = extensions[extension];
       }
     };
   }
